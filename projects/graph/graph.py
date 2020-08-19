85bd3a09765a84af39222bc78a8434d79af256c1
lesley <lesleybanadzem2017@gmail.com>
2020-08-19 14:06:57 -0700

    Completed the bft

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,9 +3,11 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,7 +15,7 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         """
@@ -21,18 +23,35 @@ class Graph:
         """
         pass  # TODO
 
+        if v1 in self.vertices:
+            self.vertices[v1].add(v2)
+
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
         pass  # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order
         beginning from starting_vertex.
         """
-        pass  # TODO
+        visited = set()
+        q = Queue()
+        if starting_vertex in self.vertices:
+            q.enqueue(starting_vertex)
+
+        while len(q.queue) > 0:
+
+            dequeue_vert = q.dequeue()
+            if dequeue_vert not in visited:
+                print(dequeue_vert)
+                visited.add(dequeue_vert)
+
+                for neighbors in self.get_neighbors(dequeue_vert):
+                    q.enqueue(neighbors)
 
     def dft(self, starting_vertex):
         """
@@ -75,6 +94,7 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
